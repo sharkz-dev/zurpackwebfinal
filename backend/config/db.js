cb860017e3c93842c2e785d1cf446c6b9cd64bda
SharKz-DEV <sharkzmc.dev@gmail.com>
2025-08-06 18:16:33 -0400

    test

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      dbName: 'zurpackdb', // <- Especifica la base de datos real de trabajo
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
